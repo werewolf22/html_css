2fd9ed09064f51fe0709192b1b8c5104b4a437a8
werewolf <hero.himal43@gmail.com>
2023-04-05 23:55:17 +0545

    fontawesome implemented, col-auto in bootstrap, change font family and background and box-shadow and opacity

--- zengarden-basic/vite.config.js
+++ zengarden-basic/vite.config.js
@@ -4,7 +4,7 @@ export default {
   root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~': path.resolve(__dirname, 'node_modules'),
     }
   },
   server: {
